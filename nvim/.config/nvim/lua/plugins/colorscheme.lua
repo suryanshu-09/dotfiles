f7b4dc2629c1b4b86bc2fd03949a621cce56e19f
suryanshu-09 <suryanshusaini@gmail.com>
2025-05-07 01:33:31 +0530

    update

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,22 @@
--- return {
--- 	-- add gruvbox
--- 	{ "ellisonleao/gruvbox.nvim" },
---
--- 	-- Configure LazyVim to load gruvbox
--- 	{
--- 		"LazyVim/LazyVim",
--- 		opts = {
--- 			colorscheme = "gruvbox",
--- 		},
--- 	},
--- }
 return {
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	-- add gruvbox
+	{ "ellisonleao/gruvbox.nvim" },
+
+	-- Configure LazyVim to load gruvbox
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "catppuccin-macchiato",
+			colorscheme = "gruvbox",
 		},
 	},
 }
+
+-- return {
+-- 	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+-- 	{
+-- 		"LazyVim/LazyVim",
+-- 		opts = {
+-- 			colorscheme = "catppuccin-macchiato",
+-- 		},
+-- 	},
+-- }
